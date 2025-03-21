60ab067abb7aef7db51d43e6dde428cdfee7dcd0
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2025-03-21 11:09:41 +0630

    report updated by amk

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
+         Commands\PullReport::class,
+
     ];
 
     /**
@@ -15,17 +17,20 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        
+        $schedule->command('make:pull-report')->everyFiveSeconds();
+        
         // $schedule->command('inspire')->hourly();
-        $schedule->command('summary:fetch')->dailyAt('00:01');
+        //$schedule->command('summary:fetch')->dailyAt('00:01');
         //$schedule->command('result:delete-old-backups {start_date} {end_date}');
         // Schedule the deletion of old results with static dates
-        $startDate = '2024-11-18';
-        $endDate = '2024-11-19';
-        $schedule->command("result:delete-old-backups $startDate $endDate")->dailyAt('00:30');
+        //$startDate = '2024-11-18';
+       // $endDate = '2024-11-19';
+       // $schedule->command("result:delete-old-backups $startDate $endDate")->dailyAt('00:30');
 
-        $sDate = '2024-11-18';
-        $eDate = '2024-11-19';
-        $schedule->command("bet:delete-old-backups $sDate $eDate")->dailyAt('00:30');
+       // $sDate = '2024-11-18';
+       // $eDate = '2024-11-19';
+       // $schedule->command("bet:delete-old-backups $sDate $eDate")->dailyAt('00:30');
 
     }
 
