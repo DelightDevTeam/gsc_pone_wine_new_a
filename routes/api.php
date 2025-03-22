9a7b23ab79a88ff756efd0d2b83e48e77a1f1826
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2025-03-22 08:27:38 +0630

    provider logo updated by amk

--- routes/api.php
+++ routes/api.php
@@ -89,7 +89,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     //slot
     Route::get('gameTypeProducts/{id}', [GameController::class, 'gameTypeProducts']);
     Route::get('allGameProducts', [GameController::class, 'allGameProducts']);
-    Route::get('gameType', [GameController::class, 'gameType']);
+    Route::get('game_types', [GameController::class, 'gameType']);
     Route::get('hotgamelist', [GameController::class, 'HotgameList']);
     Route::get('pphotgamelist', [GameController::class, 'PPHotgameList']);
     Route::get('gamelist/{provider_id}/{game_type_id}/', [GameController::class, 'gameList']);
