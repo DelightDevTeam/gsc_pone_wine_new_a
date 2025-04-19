128688bd6f5bb6fe9ec684db53d97c16ec703ca1
wlktech <stephensuu03@gmail.com>
2025-04-19 13:49:47 +0700

    a

--- app/Http/Controllers/Api/V1/Home/HomeController.php
+++ app/Http/Controllers/Api/V1/Home/HomeController.php
@@ -58,12 +58,11 @@ class HomeController extends Controller
     public function index()
 {
     $user = Auth::user();
-    $admin = $user->parent->parent->parent->parent;
-    // return $admin;
+    $admin = $user->parent->parent->parent->parent->id;
+    return $admin;
 
     // Fetch data
     $banners = Banner::where('admin_id', $admin->agent_id)->get();
-    return $banners;
     $rewards = TopTenWithdraw::where('admin_id', $admin->agent_id)->get();
     $banner_text = BannerText::where('admin_id', $admin->agent_id)->latest()->first();
     $ads_banner = BannerAds::where('admin_id', $admin->agent_id)->latest()->first();
