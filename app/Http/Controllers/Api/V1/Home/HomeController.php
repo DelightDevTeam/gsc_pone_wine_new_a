2ff901a2b82e6a2a16587a9e3015efdb73efbef6
wlktech <stephensuu03@gmail.com>
2025-04-19 13:08:02 +0700

    a

--- app/Http/Controllers/Api/V1/Home/HomeController.php
+++ app/Http/Controllers/Api/V1/Home/HomeController.php
@@ -58,7 +58,7 @@ class HomeController extends Controller
     public function index()
 {
     $user = Auth::user();
-    $admin = $user->parent->parent->parent;
+    $admin = $user->parent->parent->parent->parent;
     return $admin;
 
     // Fetch data
